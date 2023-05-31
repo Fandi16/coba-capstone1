aa65fd769e0938a1e6f4275c8b2ec1418d077c28
Fandi 16 <fanditok16@gmail.com>
2023-05-31 12:53:45 +0700

    update

--- src/script/index.js
+++ src/script/index.js
@@ -1,4 +1,23 @@
 import 'regenerator-runtime';
 import '../../src/styles/main.css';
 import '../../src/styles/responsive.css';
+import '../components/app-bar';
+import '../components/jumbotran';
+import '../components/footer';
 
+const hamburgerButtonElement = document.querySelector('#hamburgerButton');
+const drawerElement = document.querySelector('#drawer');
+const mainElement = document.querySelector('#menu');
+
+
+
+
+hamburgerButtonElement.addEventListener('click', (event) => {
+  drawerElement.classList.toggle('open');
+  event.stopPropagation();
+});
+
+mainElement.addEventListener('click', (event) => {
+  drawerElement.classList.remove('open');
+  event.stopPropagation();
+} );
---
+++ src/script/routes/routes.js
@@ -0,0 +1,10 @@
+import Explore from '../views/page/explore';
+import login from '../views/page/login';
+
+const routes = {
+  '/': Explore,
+  '/Explore': Explore,
+  '/login': login,
+};
+
+export default routes;
